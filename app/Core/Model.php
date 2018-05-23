cb3e764582b0dea6fc678a2897d00c6d0e065fbb
Iago Queiroz <iago.queiroz@hotmail.com>
2018-05-23 14:23:00 -0300

    Adicionado UserModel e adicionado alguns metodos na model base

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -20,13 +20,33 @@ class Model
             $options = array(
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
             );
 
             $this->db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS, $options);
 
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             exit("The application cannot connect to the database");
         }
+    }
+
+    public function find($id)
+    {
+        $sql = "SELECT * FROM {$this->table} WHERE id = :id LIMIT 1";
+        $query = $this->db->prepare($sql);
+        $params = [':id' => $id];
+
+        $query->execute($params);
+
+        return ($query->rowCount() ? $query->fetch() : false);
+    }
+
+    public function findAll()
+    {
+        $sql = "SELECT * FROM {$this->table}";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        return $query->fetchAll();
     }
 }

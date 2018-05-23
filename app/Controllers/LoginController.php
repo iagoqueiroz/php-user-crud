d44df06194668de4605906cca8209c35350092af
Iago Queiroz <iago.queiroz@hotmail.com>
2018-05-23 17:48:35 -0300

    Logando usuários verificados e logout

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -7,6 +7,11 @@ class LoginController
 {
     public function index()
     {
+
+        if(isset($_SESSION['user_logged']) && $_SESSION['user_logged'] == true){
+            header('Location: ' . URL . 'home');
+        }
+
         require_once APP . 'Views/_partials/header.php';
         require_once APP . 'Views/login/login.php';
         require_once APP . 'Views/_partials/footer.php';
@@ -14,7 +19,25 @@ class LoginController
 
     public function authenticate()
     {
-        var_dump($_POST);
+        $data = [
+            'email' => filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL),
+            'senha' => filter_input(INPUT_POST, 'password')
+        ];
+
+        $model = new UserModel;
+
+        if(!$user = $model->findByEmail($data['email'])){
+            die('Usuário não cadastrado');
+        }
+
+        if(!password_verify($data['senha'], $user->senha)){
+            die('Senha incorreta, tente novamente');
+        }
+
+        $_SESSION['user_logged'] = true;
+        $_SESSION['user_info'] = ['id' => $user->id, 'name' => $user->nome, 'email' => $user->email];
+
+        header('Location: ' . URL . 'home');
     }
 
     public function register()
@@ -33,19 +56,24 @@ class LoginController
             'data_nascimento' => filter_input(INPUT_POST, 'birthday')
         ];
 
-        $user = new UserModel;
+        $model = new UserModel;
 
-        if($user->findByEmail($data['email'])) {
+        if($model->findByEmail($data['email'])) {
             die('Usuário já existente');
         }
 
-        if($user->create($data)) {
+        if($model->create($data)) {
             header('Location: ' . URL . 'login');
         }
     }
 
     public function logout()
     {
-        //
+        if($_SESSION['user_logged']) {
+            unset($_SESSION['user_logged']);
+            unset($_SESSION['user_info']);
+
+            header('Location: ' . URL . 'login');
+        }
     }
 }
